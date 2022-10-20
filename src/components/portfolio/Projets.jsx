7116b5518f850dbfbfeb377a757e11745cee124d
Sylvain <sipixer@gmail.com>
2022-10-20 14:47:38 +0200

    Update Projets

--- src/components/portfolio/Projets.jsx
+++ src/components/portfolio/Projets.jsx
@@ -1,9 +1,15 @@
-import Image1 from "../../assets/Capture.png";
+import Image1 from "../../assets/Portfolio.png";
+import Image2 from "../../assets/QR-code.png";
 const Projets = [
   {
     img: Image1,
     title: "Mon Portfolio",
     link: "#",
+  },
+  {
+    img: Image2,
+    title: "Frontend Mentor | QR code component",
+    link: "./qr",
   },
 ];
 
